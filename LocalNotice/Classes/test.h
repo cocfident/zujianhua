640778bd612ed115667cc35a07b6d40e633156d7
Chongzi <chongzi@ChongzideiMac.local>
2017-07-10 15:49:37 +0800

    2

---
+++ LocalNotice/Classes/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  LocalNotice
+//
+//  Created by Chongzi on 2017/7/10.
+//  Copyright © 2017年 Chongzi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
++ (void)log;
+@end
---
+++ LocalNotice/Classes/test.m
@@ -0,0 +1,15 @@
+//
+//  test.m
+//  LocalNotice
+//
+//  Created by Chongzi on 2017/7/10.
+//  Copyright © 2017年 Chongzi. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
++(void)log{
+    NSLog(@"1111");
+}
+@end
